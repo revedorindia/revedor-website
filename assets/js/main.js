502cfd01a9834b40c3c9ab61982be99ca1d82d86
Aman Sonwani <amansonwani43@gmail.com>
2021-08-23 11:31:33 +0530

    navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,7 +108,7 @@ function togglePopup2(){
         main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
       }
       if (cur_pos < 300) {
-        $(".nav-menu ul:first li:first").addClass('active');
+        $(".nav-menu ul:first li:last").addClass('active');
       }
     });
   });
